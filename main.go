70d8575a829ea98f8e797e1b8dfe5137c2e0573b
cevhyruz <johnfred.fadrigalan@hotmail.com>
2019-01-24 15:31:23 +0800

    Initial commit.

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+  "github.com/cevhyruz/go"
+)
+
+func main() {
+  fmt.Println("hello world from main.go")
+}
---
+++ src/test.go
@@ -0,0 +1,9 @@
+package test
+
+import (
+  "fmt"
+)
+func EditBox() {
+  fmt.Println("from src/test")
+}
+
